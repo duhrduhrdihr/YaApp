a53dbc25663203827b6292a9defc85ed421642f3
rodrigoveraa <rodrigoeveraa@gmail.com>
2020-04-28 13:14:10 -0400

    GUI no longer freezes when playing yaaap

--- src/YaApp.py
+++ src/YaApp.py
@@ -1,7 +1,15 @@
 from tkinter import *
 from tkinter import ttk
+from concurrent import futures
 
 from audio_playback import play_yaaap
+
+
+tpe = futures.ThreadPoolExecutor()
+
+
+def on_button():
+    tpe.submit(play_yaaap)
 
 
 root = Tk()
@@ -13,5 +21,6 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-ttk.Button(mainframe, text="Yaaap", command=play_yaaap).grid(column=1)
+# Yaaap playblack is handled on a separate thread, so the GUI doesn't freeze
+ttk.Button(mainframe, text="Yaaap", command=on_button).grid(column=1)
 root.mainloop()
