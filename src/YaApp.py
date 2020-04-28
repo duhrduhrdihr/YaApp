a052d76d1a26976ba2393cd3604e15958f25d0bb
rodrigoveraa <rodrigoeveraa@gmail.com>
2020-04-28 14:06:00 -0400

    Added icon

--- src/YaApp.py
+++ src/YaApp.py
@@ -1,11 +1,15 @@
 from tkinter import *
 from tkinter import ttk
 from concurrent import futures
+from os import path
 
 from audio_playback import play_yaaap
 
 
 tpe = futures.ThreadPoolExecutor()
+
+bundle_dir = getattr(sys, '_MEIPASS', path.abspath(path.dirname(__file__)))
+RESOURCES_ROOT = path.join(bundle_dir, 'resources')
 
 
 def on_button():
@@ -14,6 +18,7 @@ def on_button():
 
 root = Tk()
 root.title("YaApp")
+root.iconbitmap(path.join(RESOURCES_ROOT, "yaaap_icon.ico"))
 
 mainframe = ttk.Frame(root, padding="75 12 75 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
